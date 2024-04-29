04adc5ae77bd6c993d9922bfaaafc6be1013b2dd
Demerrick Moton <dmoton3@gatech.edu>
2024-04-29 13:28:40 -0500

    update dagster uni project

--- dagster_university/orchestration/dagster_university/dagster_university/__init__.py
+++ dagster_university/orchestration/dagster_university/dagster_university/__init__.py
@@ -2,14 +2,10 @@
 from dagster import Definitions, load_assets_from_modules
 
 from .assets import metrics, trips
-from .resources import database_resource
 
 trip_assets = load_assets_from_modules([trips])
 metric_assets = load_assets_from_modules([metrics])
 
 defs = Definitions(
-    assets=[*trip_assets, *metric_assets],
-    resources={
-        "database": database_resource,
-    }
+    assets=[*trip_assets, *metric_assets]
 )
